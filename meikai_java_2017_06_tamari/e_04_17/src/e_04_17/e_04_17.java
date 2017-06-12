cc61a7f7366587758048202d655e0b86edcef348
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-12 11:46:19 +0900

    e_04_17の修正版コミット

--- meikai_java_2017_06_tamari/e_04_17/src/e_04_17/e_04_17.java
+++ meikai_java_2017_06_tamari/e_04_17/src/e_04_17/e_04_17.java
@@ -15,15 +15,14 @@ public class e_04_17 {
 		System.out.print("整数値：");
 		// nをキーボードから入力
 		int n = stdIn.nextInt();
-		// 約数の個数を数える
-		int count = 0;
+		int count = 0;// 約数の個数
 		// iがn以下の時繰り返し、iは一つずつ増やす
 		for (int i = 1; i <= n; i++) {
-			// ｎをiで割った余りが０の時
+			// ｎをiが割り切れるとき
 			if (n % i == 0) {
 				// その時のiを表示
 				System.out.println(i);
-				// countを一つ増やす
+				// 約数の数を一つ増やす
 				count++;
 			}
 		}
