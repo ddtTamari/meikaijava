c7b02302ae4d11762ccab8b8e082eea625b08701
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-12 15:58:04 +0900

    e_06_14のコミット

--- meikai_java_2017_06_tamari/e_06_14/src/e_06_14/e_06_14.java
+++ meikai_java_2017_06_tamari/e_06_14/src/e_06_14/e_06_14.java
@@ -5,34 +5,42 @@
   製作者		玉利仁美
  */
 package e_06_14;
+
 import java.util.Random;
 import java.util.Scanner;
-public interface e_06_14 {
-	public static void main(String[] args){
-		Random rand =new Random();
+
+public class e_06_14 {
+	public static void main(String[] args) {
+		Random rand = new Random();
 		Scanner stdIn = new Scanner(System.in);
 
-		String[] monthAnser ={
-				"January" , "February" , "March" , "April" , "May" , "Jun" ,
-				"July","August" , "Septenmber" , "Octorber" , "November" , "December"
-		};
-		int monthQue ;
+		String[] monthAnswer = { "January", "February", "March", "April", "May", "Jun", "July", "August", "September",
+				"Octorber", "November", "December" };
+		int monthQue;
+		int retry = 1;
+		int question=0;
+		String Answer;
 		System.out.print("英語の月名を入力してください。\nなお、先頭は大文字で、2文字目以降は小文字とします。\n");
 
+		while (retry == 1) {
+			do {
+				monthQue = rand.nextInt(12);
 
-		while(true){
-			monthQue = rand.nextInt(12);
-			System.out.print(monthQue + "月：");
-			String Answer = stdIn.nextLine();
-
-			if(Answer.equals(monthAnser[monthQue])) break;
+			}while(monthQue == question);
+			System.out.print(monthQue + 1 + "月：");
+			Answer = stdIn.next();
+			question = monthQue;
+			if (Answer.equals(monthAnswer[monthQue])) {
+				// System.out.println(monthAnswer[monthQue]);
+				System.out.println("正解です。");
+			} else if (Answer.equals(monthAnswer[monthQue]) == false) {
 				System.out.println("違います。");
+			}
+
+			System.out.print("もう一度？　1…Yes / 0…No :");
+			retry = stdIn.nextInt();
+
 		}
-		System.out.println("正解です。");
-
-
-		}
-
-
 	}
+
 }
