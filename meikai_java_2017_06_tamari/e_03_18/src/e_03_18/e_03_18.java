b304d3dbb2c8b5e0d9ea9ed4e604898a48a0e5d2
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-12 11:17:31 +0900

    e_03_18の修正版コミット

--- meikai_java_2017_06_tamari/e_03_18/src/e_03_18/e_03_18.java
+++ meikai_java_2017_06_tamari/e_03_18/src/e_03_18/e_03_18.java
@@ -16,35 +16,21 @@ public class e_03_18 {
 
 		switch (month) {
 		case 1:
-			System.out.println("冬");
-			break;
 		case 2:
 			System.out.println("冬");
 			break;
 		case 3:
-			System.out.println("春");
-			break;
 		case 4:
-			System.out.println("春");
-			break;
 		case 5:
 			System.out.println("春");
 			break;
 		case 6:
-			System.out.println("夏");
-			break;
 		case 7:
-			System.out.println("夏");
-			break;
 		case 8:
 			System.out.println("夏");
 			break;
 		case 9:
-			System.out.println("秋");
-			break;
 		case 10:
-			System.out.println("秋");
-			break;
 		case 11:
 			System.out.println("秋");
 			break;
