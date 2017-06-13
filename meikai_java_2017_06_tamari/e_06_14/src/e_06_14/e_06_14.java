d754c3129d6625fb0c0a606e9f5b6614a897fd0c
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-13 14:16:45 +0900

    e_06_14

--- meikai_java_2017_06_tamari/e_06_14/src/e_06_14/e_06_14.java
+++ meikai_java_2017_06_tamari/e_06_14/src/e_06_14/e_06_14.java
@@ -14,32 +14,43 @@ public class e_06_14 {
 		Random rand = new Random();
 		Scanner stdIn = new Scanner(System.in);
 
+		//月の英語表記を入れるための配列を用意
 		String[] monthAnswer = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
 				"Octorber", "November", "December" };
-		int monthQue;
-		int retry = 1;
-		int question = 0;
-		String Answer;
+		int monthQue;	//問題に出すのが何月かの変数
+		int retry = 1;//繰り返し行うかどうかの判定に利用
+		int question = 12;//ひとつ前の問題を入れておくための変数、初回に1月が出るように12を代入
+		String Answer;	//答えを入力するための変数
+		//問題文を表示
 		System.out.print("英語の月名を入力してください。\nなお、先頭は大文字で、2文字目以降は小文字とします。\n");
 
+		//学習者が１を押す限り繰り返される
 		while (retry == 1) {
+			//whileの条件が満たされている間行う
 			do {
+				//問題に出す月をランダムで表示
 				monthQue = rand.nextInt(12);
-
+			//問題を出す月がひとつ前の問題と同じ間繰り返す
 			} while (monthQue == question);
+			//1～12の間で問題が出るよう表示
 			System.out.print(monthQue + 1 +"月：");
+			//答えをキーボードから取得
 			Answer = stdIn.next();
+			//今出た問題をquestionとして保存
 			question = monthQue;
 
+			//入力した答えと、要素に入っている英語表記が一緒ならば
 			if (Answer.equals(monthAnswer[monthQue])) {
-				// System.out.println(monthAnswer[monthQue]);
+				//正解したことを表示
 				System.out.println("正解です。");
+			//入力した答えと、要素に入っている英語表記が違うならば
 			} else if (Answer.equals(monthAnswer[monthQue]) == false) {
-				// System.out.println(monthAnswer[monthQue]);
+				//間違っていることを表示
 				System.out.println("違います。");
 			}
-
+			//もう一度やるかどうかを聞く
 			System.out.print("もう一度？　1…Yes / 0…No :");
+			//リトライに入る数字によって繰り返すかどうか判断する
 			retry = stdIn.nextInt();
 
 		}
