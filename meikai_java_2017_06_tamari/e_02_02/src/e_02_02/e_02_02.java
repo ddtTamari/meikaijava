e1a3b30a60b27cb5a13dbefff7b826b1ba3ff01b
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-14 11:07:05 +0900

    e_02_02の修正版コミット ・変数の名前の変更とコメントの修正

--- meikai_java_2017_06_tamari/e_02_02/src/e_02_02/e_02_02.java
+++ meikai_java_2017_06_tamari/e_02_02/src/e_02_02/e_02_02.java
@@ -8,19 +8,19 @@ package e_02_02;
 public class e_02_02 {
 	public static void main(String[] args) {
 
-		int x; // xはint型の変数
-		int y; // ｙはint型の変数
-		int z; // ｚはint型の変数
+		int integerA; // integerAはint型の合計と平均を求める3値のうちの一つ目
+		int integerB; // integerBはint型の合計と平均を求める3値のうちの二つ目
+		int integerC; // integerCはint型の合計と平均を求める3値のうちの三つ目
 
-		x = 63;
-		y = 18;
-		z = 35; // x,y,zにそれぞれ63、18、35を代入
+		integerA = 63;	//integerAに63を代入
+		integerB = 18;	//integerBに18を代入
+		integerC = 35; 	//integerCに35を代入
 
-		// x,y,zの合計を求める
-		System.out.println("合計は" + (x + y + z));
+		// integerA,integerB,integerCの合計を求め、その値を表示
+		System.out.println("合計は" + (integerA + integerB + integerC));
 
-		// x,y,zの平均を求める
-		System.out.println("平均は" + (x + y + z) / 2);
+		// integerA,integerB,integerCの平均を求め、その値を表示
+		System.out.println("平均は" + (integerA + integerB + integerC) / 2);
 	}
 
 }
