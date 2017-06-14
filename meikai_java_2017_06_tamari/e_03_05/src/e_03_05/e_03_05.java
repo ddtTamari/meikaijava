b40f0ee72d17fb05255552f59a1e1b6a6217fb44
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-14 16:02:38 +0900

    e_03_05の修正版コミット ・変数名、コメント、ストリームの解放などを修正しました。

--- meikai_java_2017_06_tamari/e_03_05/src/e_03_05/e_03_05.java
+++ meikai_java_2017_06_tamari/e_03_05/src/e_03_05/e_03_05.java
@@ -9,24 +9,29 @@ import java.util.Scanner;
 
 public class e_03_05 {
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
-		// 整数値の入力を促す
+		// 5の数字で割り切れる正の整数値かどうかの判定をする整数値の入力を促す
 		System.out.print("整数値：");
-		// 変数ｎにキーボードから入力
-		int n = stdIn.nextInt();
+		// 5の倍数であるかどうか判定する正の整数値の値をキーボードから取得する
+		int integer = scan.nextInt();
 
-		// ｎが正の数字の時
-		if (n > 0)
-			// ｎを5で割った余りが0ならば、5で割り切れますと表示
-			if (n % 5 == 0)
+		// 入力された整数値が正の値の時のみ、5で割り切れるか判定する
+		if (integer > 0) {
+			// 入力された整数値が5で割り切れるならば、5で割り切れると表示する
+			if (integer % 5 == 0) {
+				// 5で割り切れたので、5で割り切れますと表示
 				System.out.print("その値は5で割り切れます。");
-			// 5で割り切れないならば、5で割り切れないと表示
-			else
+				// 5で割り切れないならば、5で割り切れないと表示
+			} else {
 				System.out.print("その値は5で割り切れません。");
-		// ｎが正の数字でない時、正ではない値が入力されたと表示
-		else
+			}
+		// ｎが正の値でない時、5で割り切れるか判定はしないので、正の値ではありませんと表示、
+		} else {
+			//正の値ではないので、正ではない値が入力されたと表示
 			System.out.print("正ではない値が入力されました。");
-
+		}
+		//ストリームを解放する
+		scan.close();
 	}
 }
