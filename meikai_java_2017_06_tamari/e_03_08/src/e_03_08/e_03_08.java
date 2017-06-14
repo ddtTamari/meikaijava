aa51a84c3f57c3bec1f7c088e7b359a624f72d02
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-14 17:03:09 +0900

    e_03_08の修正版コミット ・コメント、ストリームの解放などを修正しました。

--- meikai_java_2017_06_tamari/e_03_08/src/e_03_08/e_03_08.java
+++ meikai_java_2017_06_tamari/e_03_08/src/e_03_08/e_03_08.java
@@ -11,26 +11,36 @@ import java.util.Scanner;
 public class e_03_08 {
 
 	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 
+		//評価を行う点数について入力を促す
 		System.out.print("点数：");
-		int score = stdIn.nextInt();
+		//点数はキーボードから取得する
+		int score = scan.nextInt();
 
-		// 点数が0～59点の場合、不可
-		if (score >= 0 && score < 60)
+		// 点数が0～59点の場合、不可と表示
+		if (score >= 0 && score < 60) {
+			//0～59点のいずれかだったので不可と表示
 			System.out.println("不可");
-		// 点数が60~69点の場合、可
-		else if (score >= 60 && score < 70)
+		// 点数が60~69点の場合、可と表示
+		} else if (score >= 60 && score < 70) {
+			//60～69点のいずれかだったので可と表示
 			System.out.println("可");
-		// 点数が70~79点の場合、良
-		else if (score >= 70 && score < 80)
+		// 点数が70~79点の場合、良と表示
+		} else if (score >= 70 && score < 80) {
+			//70～79点のいずれかだったので良と表示
 			System.out.println("良");
-		// 点数が80~100点の場合、優
-		else if (score >= 80 && score <= 100)
+		// 点数が80~100点の場合、優と表示
+		} else if (score >= 80 && score <= 100) {
+			//80～100点の場合、優と表示
 			System.out.println("優");
-		// それ以外の点数の場合、判定外
-		else
+		//0～100ではない値が入力されたら判定ができないので判定外と表示
+		} else {
+			//0～100ではない数字が入力されたので、判定外と表示
 			System.out.println("判定外");
+		}
+		//ストリームを解放
+		scan.close();
 
 	}
 }
