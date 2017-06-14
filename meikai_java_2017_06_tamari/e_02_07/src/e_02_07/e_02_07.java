2a62788b357021186e9507684adab6433389a344
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-14 14:51:29 +0900

    e_02_07の修正版コミット ・変数名、コメントの修正を行いました。

--- meikai_java_2017_06_tamari/e_02_07/src/e_02_07/e_02_07.java
+++ meikai_java_2017_06_tamari/e_02_07/src/e_02_07/e_02_07.java
@@ -12,17 +12,17 @@ public class e_02_07 {
 	public static void main(String[] args) {
 		Random rand = new Random();
 
-		// 変数ｘにランダムで0～9の数字を代入
-		int x = rand.nextInt(9);
-		// 0~8の数字に足す１することで正の整数値をランダムに表示
-		System.out.println(x + 1);
-		// 0~8の数字に-9することで負の整数値をランダムに表示
-		System.out.println(x - 9);
+		// 整数値に0～8の値を代入
+		int integer = rand.nextInt(9);
+		// 0～8のランダムな値に１を足すことで、1～9の正の整数値をランダムに表示
+		System.out.println(integer + 1);
+		// 0～8のランダムな値に９引くことで、-9～-1の負の整数値をランダムに表示
+		System.out.println(integer - 9);
 
-		// 変数yに0~89の数字を代入
-		int y = rand.nextInt(90);
-		// 0～89の数字に10を足し2桁の正の整数値をランダムに生成
-		System.out.println(y + 10);
+		// 2桁の正の整数値をランダムに生成するため0～89の値を代入
+		int twoDigitsInteger = rand.nextInt(90);
+		// 0～89の数字に10を足すことで、2桁の正の整数値をランダムに生成、表示
+		System.out.println(twoDigitsInteger + 10);
 
 	}
 }
