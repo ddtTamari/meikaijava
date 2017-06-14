e0f398622cbc6e01b5b3be9ee0e5f5392d00e555
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-14 10:59:53 +0900

    e_02_01の修正版コミット ・変数の名前とコメントの修正を行いました。

--- meikai_java_2017_06_tamari/e_02_01/src/e_02_01/e_02_01.java
+++ meikai_java_2017_06_tamari/e_02_01/src/e_02_01/e_02_01.java
@@ -9,19 +9,19 @@ package e_02_01;
 public class e_02_01 {
 	public static void main(String[] args) {
 
-		double decimalA; // decimalAはint型の変数
-		double decimalB; // decimalBはint型の変数
+		double decimalA; // decimalAはdouble型の2値のうちの一つ目
+		double decimalB; // decimalBはdouble型の2値のうちの二つ目
 
 		decimalA = 63.1; // ｘに小数部を持つ実数値を代入
 		decimalB = 18.2; // decimalに少数部を持つ実数値を代入
 
-		// decimalの値を表示
+		// decimalAの値を表示
 		System.out.println("decimalAの値は" + decimalA + "です。");
-		// ｙの値を表示
+		// decimalBの値を表示
 		System.out.println("decimalBの値は" + decimalB + "です。");
-		// ｘ+ｙの結果を表示
+		// decimalAとdecimalBの合計の値を表示
 		System.out.println("合計は" + (decimalA + decimalB) + "です。");
-		// ｘとｙの平均を表示
+		// decimalAとdecimalBの平均の値を表示
 		System.out.println("平均は" + (decimalA + decimalB) / 2 + "です。");
 
 	}
