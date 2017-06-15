d96ee6bf26a9915e79b38124abec7b231d8a2745
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-15 19:50:39 +0900

    e_07_05のコミット

---
+++ meikai_java_2017_06_tamari/e_07_05/src/e_07_05/e_07_05.java
@@ -0,0 +1,21 @@
+/*
+  演習7-5		『こんにちは。』と表示して改行するメソッドhelloを作成せよ
+  演習日		6月14日
+  製作者		玉利仁美
+ */
+package e_07_05;
+
+public class e_07_05 {
+	public static void main(String[] args){
+
+		//こんにちは。と表示し改行するメソッドを呼び出す
+		hello();
+	}
+	//こんにちは。と表示し改行するメソッド
+	static void hello(){
+		//こんにちは。と表示し改行する
+		System.out.print("こんにちは。\n");
+
+	}
+
+}
