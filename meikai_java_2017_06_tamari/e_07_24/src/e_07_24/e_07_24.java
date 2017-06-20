04e21c66d71320acd21b85dbd03025360afa25ea
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-20 13:21:47 +0900

    e_07_24の修正版コミット ・配列の長さを変数に格納しました

--- meikai_java_2017_06_tamari/e_07_24/src/e_07_24/e_07_24.java
+++ meikai_java_2017_06_tamari/e_07_24/src/e_07_24/e_07_24.java
@@ -9,42 +9,45 @@ import java.util.Scanner;
 
 public class e_07_24 {
 	public static void main(String[] args) {
+		// キーボードから要素数などの値を取得するために宣言
 		Scanner scan = new Scanner(System.in);
 		// これから生成する配列の要素数の入力を求める
 		System.out.print("要素数：");
 		// 要素の数をキーボードから取得
 		int elements = scan.nextInt();
 		// 要素数elementsの配列aを生成
-		int[] arrayA = new int[elements];
+		int[] array1 = new int[elements];
 
 		// 要素の数だけ値の入力を求めるので、要素数分繰り返す
 		for (int i = 0; i < elements; i++) {
 			// i個目の要素の値の入力を求める
 			System.out.print("a[" + i + "]:");
 			// 配列のi個目の値をキーボードから取得
-			arrayA[i] = scan.nextInt();
+			array1[i] = scan.nextInt();
 		}
 
 		// 何個目の要素の値を消すか入力を求める
 		System.out.print("何個目の値を消しますか：");
 		// 何個目の要素を消すのかキーボードからインデックス番号を取得
 		int index = scan.nextInt();
+		//arrayRmvOfメソッドで作られた配列の長さを格納しておく
+		int arrayRmvOfLong = arrayRmvOf(array1, index).length;
 
 		// 配列の要素数を表示するために繰り返し行う
-		for (int i = 0; i < arrayRmvOf(arrayA, index).length; i++) {
+		for (int i = 0; i < arrayRmvOfLong; i++) {
 			// 入力したindexの値を削除した配列を返却するメソッドを呼び出し、その値を表示する
-			System.out.println("a[" + i + "]:" + arrayRmvOf(arrayA, index)[i]);
+			System.out.println("a[" + i + "]:" + arrayRmvOf(array1, index)[i]);
 		}
+
 		// ストリームを解放する
 		scan.close();
-
 	}
 
-	//実引数のidx番目の要素を削除した配列を生成するメソッド
+	// 実引数のidx番目の要素を削除した配列を生成するメソッド
 	static int[] arrayRmvOf(int[] a, int idx) {
-		//配列aの長さを変数として保管
+		// 配列aの長さを変数として保管
 		int aLong = a.length;
-		//配列arrayRemoveは配列aの要素を一つ削除しているので一つ少ない要素数になる
+		// 配列arrayRemoveは配列aの要素を一つ削除しているので一つ少ない要素数になる
 		int[] arrayRemove = new int[aLong - 1];
 
 		// 配列の長さの分だけ繰り返し行う
@@ -54,13 +57,13 @@ public class e_07_24 {
 			if (i > idx) {
 				// ひとつ前の要素に今の要素の値を代入する
 				arrayRemove[i - 1] = a[i];
-			//消す要素より前の要素はそのままarrayRemoveに入る
+				// 消す要素より前の要素はそのままarrayRemoveに入る
 			} else {
-				//配列arrayRemoveに配列aのi番目の要素を代入する
+				// 配列arrayRemoveに配列aのi番目の要素を代入する
 				arrayRemove[i] = a[i];
 			}
 		}
-		//配列arrayRemoveを返却する
+		// 配列arrayRemoveを返却する
 		return arrayRemove;
 	}
 }
