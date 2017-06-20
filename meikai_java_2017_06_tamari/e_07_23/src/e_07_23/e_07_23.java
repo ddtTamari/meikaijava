b74b4343055c66c4dbbe6b1a8c090700ac7527fc
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-20 13:15:44 +0900

    e_07_23の修正版コミット ・コメント、改行、配列名など修正を行いました

--- meikai_java_2017_06_tamari/e_07_23/src/e_07_23/e_07_23.java
+++ meikai_java_2017_06_tamari/e_07_23/src/e_07_23/e_07_23.java
@@ -9,31 +9,32 @@ import java.util.Scanner;
 
 public class e_07_23 {
 	public static void main(String[] args) {
+		// キーボードから値を取得するために宣言
 		Scanner scan = new Scanner(System.in);
 
 		// クローンを作るもととなる配列aの要素の数の入力を求める
 		System.out.print("要素数：");
 		// 要素数をキーボードから取得
-		int elementsA = scan.nextInt();
+		int elements = scan.nextInt();
 		// 入力された要素数を持った配列aを生成
-		int[] arrayA = new int[elementsA];
+		int[] array = new int[elements];
 
 		// すべての要素を表示するため要素数回分繰り返し行う
-		for (int i = 0; i < elementsA; i++) {
+		for (int i = 0; i < elements; i++) {
 			// i個目の要素の値の入力を促す
 			System.out.print("a[" + i + "]:");
 			// 要素の値をキーボードから取得する
-			arrayA[i] = scan.nextInt();
+			array[i] = scan.nextInt();
 		}
-		// 配列Aの中を探す値の入力を促す
+		// 配列の中を探す値の入力を促す
 		System.out.print("探す値：");
 		// 探す値をキーボードから取得する
 		int key = scan.nextInt();
 
 		// すべての要素を表示するために要素数分繰り返し行う
-		for (int i = 0; i < arraySrchIdx(arrayA, key).length; i++) {
+		for (int i = 0; i < arraySrchIdx(array, key).length; i++) {
 			// 配列aの要素と探す値が一致したときの配列aの要素番号を格納する配列を作るメソッドを呼び出し、その値を表示する
-			System.out.println("key[" + i + "]:" + arraySrchIdx(arrayA, key)[i]);
+			System.out.println("key[" + i + "]:" + arraySrchIdx(array, key)[i]);
 
 		}
 		// ストリームを解放する
@@ -46,6 +47,7 @@ public class e_07_23 {
 		int elementsKey = 0;
 		// 配列aの長さを記憶しておく
 		int aLong = a.length;
+
 		// 配列aの中に何個、探す値があるのか調べたいので要素数分繰り返す
 		for (int i = 0; i < aLong; i++) {
 			// i個目の要素と探す値が一緒の時、それを格納する要素数を一つ増やす
@@ -58,6 +60,7 @@ public class e_07_23 {
 		int elements = 0;
 		// 要素数がelementsKey(a[i]とkeyが同じになった回数)の配列keyを生成
 		int[] arrayKey = new int[elementsKey];
+
 		// 配列aの長さ繰り返す
 		for (int i = 0; i < aLong; i++) {
 			// 探している値と配列aのi個目の要素の値が一緒だった場合
