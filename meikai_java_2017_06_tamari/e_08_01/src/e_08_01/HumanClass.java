eb55f50016468efab66c76e294df987d8ea16f97
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-21 15:24:47 +0900

    e_08_01のコミット

--- meikai_java_2017_06_tamari/e_08_01/src/e_08_01/HumanClass.java
+++ meikai_java_2017_06_tamari/e_08_01/src/e_08_01/HumanClass.java
@@ -1,21 +1,26 @@
+/*
+  演習8-1		名前・身長・体重などをメンバとして持つ《人間クラス》を作成せよ(フィールドやメソッドなどは自分で自由に設計すること)
+  演習日		6月21日
+  製作者		玉利仁美
+ */
 package e_08_01;
 
+//表示する人間のそれぞれのインスタンスの値を表示する
 public class HumanClass {
+	// 名前を表すフィールド
 	private String name;
+	// 年を表すフィールド
 	private int age;
+	// 性別を表すフィールド
 	private String gender;
+	// 国籍を表すフィールド
 	private String nationality;
+	// 身長を表すフィールド
 	private float height;
+	// 体重を表すフィールド
 	private float weight;
 
-	final String Human_Name = new String("名前：");
-	final String Age = new String("年齢：");
-	final String Gender = new String("性別：");
-	final String Nationality = new String("国籍：");
-	final String Height = String.format("身長：%3.1fcm", this.height);
-	final String Weight = String.format("体重：%3.1fkg", this.weight);
-	final String BMI_Index = new String("ボディマス指数：");
-
+	// 名前、年、性別、国籍、身長、体重この6つの値でクラスのインスタンスを初期化させる、
 	HumanClass(String name, int age, String gender, String nationality, float height, float weight) {
 		this.name = name;
 		this.age = age;
@@ -25,19 +30,29 @@ public class HumanClass {
 		this.weight = weight;
 	}
 
+	//BMIを求めるメソッド
 	float getBMI() {
+		//BMIは体重割る身長を100で割った値の2乗で求められる
 		float BMIIndex = weight / (height / 100) / (height / 100);
 		return BMIIndex;
 	}
 
 	void putHumanSpec() {
-		System.out.println(Human_Name + name);
-		System.out.println(Age + age);
-		System.out.println(Gender + gender);
-		System.out.println(Nationality + nationality);
-		System.out.println(Height + height);
-		System.out.println(Weight + weight);
-		System.out.println(BMI_Index + getBMI());
+		String Height = String.format("身長：%4.1fcm", height);
+		String Weight = String.format("身長：%4.1fcm", weight);
+
+		// 人物の名前を表示するようオブジェクトにメッセージを送る
+		System.out.println(Constant_Class.Human_Name + name);
+		// 年齢を表示するよう、定数クラスのオブジェクトにメッセージを送る
+		System.out.println(Constant_Class.Age + age);
+		// 性別を表示するよう、定数クラスのオブジェクトにメッセージを送る
+		System.out.println(Constant_Class.Gender + gender);
+		// 国籍を表示するよう、定数クラスのオブジェクトにメッセージを送る
+		System.out.println(Constant_Class.Nationality + nationality);
+		//
+		System.out.println(Height);
+		System.out.println(Weight);
+		System.out.println(Constant_Class.BMI_Index + getBMI());
 	}
 
 }
--- meikai_java_2017_06_tamari/e_08_01/src/e_08_01/e_08_01.java
+++ meikai_java_2017_06_tamari/e_08_01/src/e_08_01/e_08_01.java
@@ -6,15 +6,21 @@
 package e_08_01;
 
 public class e_08_01 {
+	//メインメソッド
 	public static void main(String[] args) {
+
+		//畠山のインスタンスを以下の値で初期化する
 		HumanClass hatakeyama = new HumanClass("畠山和洋", 34, "男", "日本", 180.0f, 96.0f);
+		//バレンティンのインスタンスを以下の値で初期化する
 		HumanClass balentien = new HumanClass("ウラディミール・バレンティン", 32, "男", "オランダ", 185.0f, 100.0f);
+		//吉田のインスタンスを以下の値で初期化する
 		HumanClass yoshida = new HumanClass("吉田沙保里", 34, "女", "日本", 156.0f, 55.0f);
 
+		//hatakeyamaのインスタンス変数の値を表示するメソッドを呼び出す
 		hatakeyama.putHumanSpec();
-		System.out.println();
+		//balentienのインスタンス変数の値を表示するメソッドを呼び出す
 		balentien.putHumanSpec();
-		System.out.println();
+		//yoshidaのインスタンス変数の値を表示するメソッドを呼び出す
 		yoshida.putHumanSpec();
 
 	}
