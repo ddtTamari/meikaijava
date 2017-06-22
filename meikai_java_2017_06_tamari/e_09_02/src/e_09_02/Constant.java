94554fb229bd4ade4493ccd3c2cf5850e55e2f5e
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-22 14:23:33 +0900

    e_09_02のコミット

---
+++ meikai_java_2017_06_tamari/e_09_02/src/e_09_02/Constant.java
@@ -0,0 +1,21 @@
+/*
+  演習9-2		日付クラス第3版を利用するプログラムを作成せよ。すべてのコンストラクタの働きを確認できるようにすること。
+  演習日		6月22日
+  製作者		玉利仁美
+ */
+package e_09_02;
+
+//文字列などの定数を保管しておくクラス
+public class Constant {
+	// 曜日を求める日付の入力を促す際使用する
+	public static final String Imput_Date = new String("日付を入力してください");
+	// 曜日を求める日付の年の入力を促す際使用する
+	public static final String What_Year = new String("年：");
+	// 曜日を求める日付の月の入力を促す際使用する
+	public static final String What_Month = new String("月：");
+	// 曜日を求める日付の日にちの入力を促す際使用する
+	public static final String What_Day = new String("日：");
+	// 何個目のコンストラクタの働きを確認しているかを表示する際に使用する
+	public static final String What_Number_Constructor = new String("個目のコンストラクタの実行例");
+
+}
