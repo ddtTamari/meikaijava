9ac00ab6878b05437212c1ffccf7ac15b867d0e7
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-22 18:04:08 +0900

    e_09_02の修正版コミット ・コメントの追加 ・消えたデータの修復

--- meikai_java_2017_06_tamari/e_09_02/src/e_09_02/Constant.java
+++ meikai_java_2017_06_tamari/e_09_02/src/e_09_02/Constant.java
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
-package e_09_02;
-
-public class Constant {
-=======
 /*
   演習9-2		日付クラス第3版を利用するプログラムを作成せよ。すべてのコンストラクタの働きを確認できるようにすること。
   演習日		6月22日
   製作者		玉利仁美
  */
+
 package e_09_02;
 
 //文字列などの定数を保管しておくクラス
@@ -22,6 +18,5 @@ public class Constant {
 	public static final String What_Day = new String("日：");
 	// 何個目のコンストラクタの働きを確認しているかを表示する際に使用する
 	public static final String What_Number_Constructor = new String("個目のコンストラクタの実行例");
->>>>>>> parent of b880497... e_09_02のコミット
 
 }
