a0d0824a510529e5c7183a94fff7257672d3a4f8
ADMIN\ddtji <ddt.jissyusei5@gmail.com>
2017-06-30 11:09:42 +0900

    e_07_13の修正版コミット ・posビット目を変えた値を10進数で返却するよう修正いたしました。

--- meikai_java_2017_06_tamari/e_07_13/src/e_07_13/e_07_13.java
+++ meikai_java_2017_06_tamari/e_07_13/src/e_07_13/e_07_13.java
@@ -5,11 +5,14 @@
   製作者		玉利仁美
  */
 package e_07_13;
-
+//キーボード入力をするためにスキャナークラスをインポート
 import java.util.Scanner;
 
+//メインクラス
 public class e_07_13 {
+	//メインメソッド
 	public static void main(String[] args) {
+		//キーボード入力を可能にするために宣言
 		Scanner scan = new Scanner(System.in);
 
 		// 整数ｘのnビット目を変える整数の値の入力を促す
@@ -53,73 +56,59 @@ public class e_07_13 {
 
 	// pos個目を1に変えるメソッド
 	static int set(int x, int pos) {
-		// 32ビットで表示するので32回繰り返す
-		for (int i = 31; i >= 0; i--) {
-			// 31から0までで表示し、i回目が指定したposと一緒の時1を表示する
-			if (i == pos) {
-				// posビット目なので1を表示
-				System.out.print('1');
-				// posビット目ではないとき
-			} else {
-				// 整数xの第iビットが1ならば0をそれ以外ならば1を返す
-				System.out.print(((x >>> i & 1) == 1) ? '1' : '0');
-			}
-		}
+		// 変える位置で1を使って論理和にするために1を準備する
+		int doChangePosBits = 1;
+		// posビット目を1に変えた値を返す変数
+		int returnChangeValue;
+		// posビット目だけが1の値にする
+		returnChangeValue = doChangePosBits << pos;
+
+		// posビット目が1になるよう論理和を使いポスビット目を1にする
+		returnChangeValue = (returnChangeValue | x);
+		// 32ビットで表示するメソッドでポスビット目を1にした値を32ビットで表示する
+		printBits(returnChangeValue);
+
 		// 32ビット分を表示し終えたので改行する
 		System.out.println();
-
-		return x;
-	}
-
-	// 2のshift乗を計算するメソッド
-	static int power(int shift) {
-		// 2のshift乗を入れておく変数を用意
-		int power = 1;
-		// shift回分繰り返し2をかけ続ける
-		for (int i = 0; i < shift; i++) {
-			// 2をi回分かける
-			power *= 2;
-		}
-		// 返却値として２のshift乗を返す
-		return power;
+		// posビット目を1にした値を返す
+		return returnChangeValue;
 	}
 
 	// pos個目を0に変えるメソッド
 	static int reset(int x, int pos) {
-		// 32ビットで表示するので32回繰り返す
-		for (int i = 31; i >= 0; i--) {
-			// 31から0までで表示し、i回目が指定したposと一緒の時0を表示する
-			if (i == pos) {
-				// posビット目なので0を表示
-				System.out.print('0');
-				// posビット目ではないとき
-			} else {
-				// 整数xの第iビットが1ならば1をそれ以外ならば0を返す
-				System.out.print(((x >>> i & 1) == 1) ? '1' : '0');
-			}
-		}
+		// 変える位置で1を使って論理積するために1を準備する
+		int doChangePosBits = 1;
+		// posビット目を0に変えた値を返す変数
+		int returnChangeValue;
+		// posビット目だけが1の値の補数を行いposビット目だけが0の値にする
+		returnChangeValue = ~(doChangePosBits << pos);
+		// posビット目が0になるよう論理積を使いポスビット目を1にする
+		returnChangeValue = (returnChangeValue & x);
+		// 32bitでposビット目が0になった値を表示する
+		printBits(returnChangeValue);
 		// 32ビット分を表示し終えたので改行する
 		System.out.println();
-		return x;
+		// posビット目を0にした値を返す
+		return returnChangeValue;
 	}
 
 	// pos個目を反転させるメソッド
 	static int inverse(int x, int pos) {
-		// 32ビットで表示するので32回繰り返す
-		for (int i = 31; i >= 0; i--) {
-			// 31から0までで表示し、i回目が指定したposと一緒の時反転した2進数を表示する
-			if (i == pos) {
-				System.out.print(((x >>> i & 1) == 1) ? '0' : '1');
-			} else {
-				// 整数xの第iビットが1ならば1をそれ以外ならば0を返す
-				System.out.print(((x >>> i & 1) == 1) ? '1' : '0');
-			}
-		}
+		// 変える位置で1を使って排他的論理和にするために1を準備する
+		int doChangePosBits = 1;
+		// posビット目を1に変えた値を返す変数
+		int returnChangeValue;
+		// posビット目だけが1の値にする
+		returnChangeValue = doChangePosBits << pos;
+		// posビット目が反転した値になるよう排他的論理和を使いポスビット目を反転した値にする
+		returnChangeValue = (returnChangeValue ^ x);
+		// 32bitでposビット目が反転した値を表示する
+		printBits(returnChangeValue);
 		// 32ビット分を表示し終えたので改行する
 		System.out.println();
-		// ｘの値を返す
-		return x;
+		// posビット目を1にした値を返す
+		return returnChangeValue;
 	}
 
 }
-// 自分の認識が間違えていました
+
